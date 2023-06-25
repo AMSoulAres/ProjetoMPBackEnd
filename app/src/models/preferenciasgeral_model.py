0015d3b04dd76d193110a4a4c5b8a51e27835c5c
carollingian <carolina.fdcampos@gmail.com>
2023-06-25 18:26:34 -0300

    criando classe model

--- app/src/models/preferenciasgeral_model.py
+++ app/src/models/preferenciasgeral_model.py
@@ -3,3 +3,7 @@
 """Modulos para definição do modelo"""
 from typing import Optional
 from pydantic import BaseModel
+
+class Model(BaseModel):  # pylint: disable=too-few-public-methods
+    """Modelo de preferencias """
+    NomePreferencias: str
