eff1f812c14b6e38e9338a1cef0c71147474544a
carollingian <carolina.fdcampos@gmail.com>
2023-06-25 17:43:36 -0300

    adicionando arquivo model

---
+++ app/src/models/preferenciasgeral_model.py
@@ -0,0 +1,5 @@
+# pylint: disable=no-name-in-module
+# pylint: disable=no-self-argument
+"""Modulos para definição do modelo"""
+from typing import Optional
+from pydantic import BaseModel
---
+++ app/tests/test_crud_preferenciasgeral.py
@@ -0,0 +1,8 @@
+# pylint: disable=pointless-string-statement
+"""Modulos importando o FastAPI"""
+from fastapi.testclient import TestClient
+from app.src.main import app
+
+client = TestClient(app)
+
+""" ----------------------------Teste get------------------------------------ """
