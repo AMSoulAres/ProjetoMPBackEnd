08143815cfc7c9c04464ac9b3f2ab563eccbe3b7
carollingian <carolina.fdcampos@gmail.com>
2023-06-25 20:16:10 -0300

    criando teste post

--- app/tests/test_crud_preferenciasgeral.py
+++ app/tests/test_crud_preferenciasgeral.py
@@ -18,3 +18,22 @@ def test_preferencias_geral_erro():
     response = client.get("/Preferencias/preferenciasgeral")
     assert response.status_code == 404
     assert response.json()
+
+
+""" ------------------------- TESTE POST ------------------------- """
+def test_cria_preferencias_sucesso():
+    """Teste"""
+    response = client.post("/Preferencias/criar-preferencias/Romance/admin")
+    assert response.status_code == 201
+    assert response.json() == {
+        "message": "Preferência(s) criada(s) com sucesso!"
+    }
+
+
+def test_cria_preferencias_erro_preferencia_existente():
+    """Teste"""
+    response = client.post("/Grupos/criar-grupo/qualquernome/admin")
+    assert response.status_code == 400
+    assert response.json() == {
+        "detail": "Erro: Preferência qualquernome já existe."
+    }
