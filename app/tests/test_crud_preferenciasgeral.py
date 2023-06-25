b056c4cf9eac8a2470a8d7964345fd48c7d7a2a0
carollingian <carolina.fdcampos@gmail.com>
2023-06-25 19:23:53 -0300

    criando teste get

--- app/tests/test_crud_preferenciasgeral.py
+++ app/tests/test_crud_preferenciasgeral.py
@@ -6,3 +6,15 @@ from app.src.main import app
 client = TestClient(app)
 
 """ ----------------------------Teste get------------------------------------ """
+def test_preferencias_geral_sucesso():
+    """"Teste"""
+    response = client.get("/Preferencias/preferencias-geral")
+    assert response.status_code == 200
+    assert response.json()
+
+
+def test_preferencias_geral_erro():
+    """"Teste"""
+    response = client.get("/Preferencias/preferenciasgeral")
+    assert response.status_code == 404
+    assert response.json()
