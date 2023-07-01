3461265cc7c529e56e85e468015d8e79e01775e9
AdriEve <108773502+AdriEve@users.noreply.github.com>
2023-06-30 22:41:11 -0300

    Teste chat - cria chat

--- app/tests/test_crud_chatPrivado.py
+++ app/tests/test_crud_chatPrivado.py
@@ -3,7 +3,6 @@
 from fastapi.testclient import TestClient
 from app.src.main import app
 
-
 client = TestClient(app)
 
 """ ------------------------- TESTE GET -------------------------  """
@@ -13,3 +12,17 @@ def test_busca_mensagem_por_id_sucesso():
     response = client.get("/ChatPrivado/buscar-mensagens/0/1")
     assert response.status_code == 200
     assert response.json()
+
+""" ------------------------- TESTE POST ------------------------- """
+
+def test_cria_chat_sucesso():
+    """Teste"""
+    response = client.post("/ChatPrivado/enviar-mensagem/0/2",
+                           json={
+                                    "mensagem": "testando"
+                           })
+    assert response.status_code == 201
+    assert response.json() == {
+        "message": "Mensagem enviada com sucesso!"
+    }
+
