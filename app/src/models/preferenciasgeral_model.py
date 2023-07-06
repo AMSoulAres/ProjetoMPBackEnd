79a7f31cef80762afe02c973fa2d8b17051d0953
Gabriel Amorim Soares e Silva <amorimsoares@gmail.com>
2023-07-05 21:54:34 -0300

    trocando funcionalidade de preferencias

--- app/src/models/preferenciasgeral_model.py
+++ app/src/models/preferenciasgeral_model.py
@@ -1,9 +1,8 @@
 # pylint: disable=no-name-in-module
 # pylint: disable=no-self-argument
 """Modulos para definição do modelo"""
-from typing import Optional
 from pydantic import BaseModel
 
 class PreferenciasGeralModel(BaseModel):  # pylint: disable=too-few-public-methods
     """Modelo de preferencias """
-    NomePreferencias: str
+    preferencias: list
--- app/src/preferenciasgeral_crud.py
+++ app/src/preferenciasgeral_crud.py
@@ -1,11 +1,9 @@
 """Importando módulos básicos para conexão com DBcd"""
-import json
+from typing import Optional
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
-from fastapi.exceptions import HTTPException
 from app.src.models.preferenciasgeral_model import PreferenciasGeralModel
 from app.src.config_db import bancoAtlax
-from app.src import exceptions
 
 router = APIRouter(
     prefix="/Preferencias",
@@ -13,141 +11,32 @@ router = APIRouter(
     responses={404: {"description": "Not Found"}}
 )
 
-""" ------------------------- CREATE -------------------------"""
-@router.post("/criar-preferencias/{username}")
-async def criar_preferencias(dados: PreferenciasGeralModel, username: str):
-    """Cria uma preferência se o usuário for admin e se a
-    Preferência não exisir.
-    Assertivas de Entrada: Username do usuário, para checar
-    se é admin, dados em formato json.
-
-    Assertiva de saída: A preferência é criada na lista de
-    preferência do banco de dados.
-
-    Em caso de erro retorna: 401(Usuário não é admin.),
-    409(Preferência já existe)."""
-    admin = 0
-    usuarios = bancoAtlax.reference("/Usuarios").get()
-    if username is None:
-        raise exceptions.ERRO_CAMPO
-
-    for key, usuario in usuarios.items():
-        if key == "Total":
-            break
-
-        if username == usuario['username']:
-            if usuario['id'] == 1:
-                admin = 1
-    if admin == 0:
-        raise HTTPException(
-            status_code=401,
-            detail="Erro: Usuário não é admin."
-        )
-
-    preferencias = bancoAtlax.reference("/Preferencias").get()
-    if dados.NomePreferencias is None:
-        raise exceptions.ERRO_CAMPO
-
-    for key, preferencia_existente in preferencias.items():
-        if key == "Total":
-            break
-
-        if preferencia_existente['NomePreferencias'] == dados.NomePreferencias:
-            raise HTTPException(
-                status_code=409,
-                detail=f"Erro: Preferência {preferencia_existente['NomePreferencias']} já existe."
-            )
-
-    total_id = bancoAtlax.reference("/Preferencias/Total").child("num").get()
-    body = json.loads(dados.json())
-
-    bancoAtlax.reference("/Preferencias").push(body)
-    bancoAtlax.reference("/Preferencias").child("Total").update({"num" : total_id + 1})
-
-    return JSONResponse(
-        status_code=201,
-        content={"message": "Preferência criada com sucesso!"}
-    )
-
-
-
-""" ------------------------- READ -------------------------"""
-@router.get("/preferencias-geral")
-async def preferencias_geral():
+@router.get("/lista-preferencias", response_model=Optional[PreferenciasGeralModel])
+async def get_lista_preferencias():
     """Lista Preferências Geral.
 
-    Assertiva de entrada: /preferencias-geral
+    Assertiva de entrada: não requer parâmetros de entrada
 
-    Assertiva de saída: Preferências armazenadas
-    na base de dados."""
-
-    path = bancoAtlax.reference("/Preferencias")
-    return path.get()
-
-@router.get("/lista-preferencias")
-async def lista_preferencias():
-    """Lista Preferências Geral.
-
-    Assertiva de entrada: /preferencias-geral
-
-    Assertiva de saída: Preferências armazenadas
-    na base de dados."""
-    lista_preferencias = []
+    Assertiva de saída: Lista de preferências armazenadas
+    na base de dados.
+    """
     preferencias = bancoAtlax.reference("/Preferencias").get()
-    for key, preferencia in preferencias.items():
-        if key == "Total":
-            pass
+    return preferencias
 
-        lista_preferencias.append(preferencia)
+@router.put("/atualiza-preferencias")
+async def update_preferencias(lista_preferencias: PreferenciasGeralModel):
+    """Atualiza as preferencias
+    
+    Assertiva de entrada: um objeto com uma lista de preferências, com valores atualizados
+    pelo admin
 
-    return lista_preferencias
+    Assertiva de saída: Atualiza o banco de dados com as preferências atualizadas
 
+    """
+    if lista_preferencias.preferencias == []:
+        return JSONResponse(status_code=200, content="Nada mudou...")
+    dados_atualizados = lista_preferencias.dict()
 
-
-""" ------------------------- DELETE -------------------------"""
-@router.delete("/deletar-preferencias/{username}/{nome_preferencia}")
-async def deletar_preferencias(username: str, nome_preferencia: str):
-    """Deleta uma preferência se o usuário for admin e a preferência for válida.
-    Assertivas de Entrada: Nome de usuário, dados em formato json.
-
-    Assertiva de saída: A preferência é deletada da lista de
-    preferência do banco de dados.
-
-    Em caso de erro retorna: 401(Usuário não é admin.),
-    409(Preferência não existe)."""
-    admin = 0
-    usuarios = bancoAtlax.reference("/Usuarios").get()
-    if username is None:
-        raise exceptions.ERRO_CAMPO
-
-    for key, usuario in usuarios.items():
-        if key == "Total":
-            break
-
-        if username == usuario['username']:
-            if usuario['id'] == 1:
-                admin = 1
-    if admin == 0:
-        raise HTTPException(
-            status_code=401,
-            detail="Erro: Usuário não é admin."
-        )
-
-    preferencias = bancoAtlax.reference("/Preferencias").get()
-    if nome_preferencia is None:
-        raise exceptions.ERRO_CAMPO
-
-    for key, preferencia_existente in preferencias.items():
-        if key == "Total":
-            break
-
-        if preferencia_existente['NomePreferencias'] == nome_preferencia:
-            bancoAtlax.reference("/Preferencias").child(str(key)).delete()
-            return JSONResponse(
-                status_code=200,
-                content={"message": "Preferência deletada com sucesso!"}
-            )
-    raise HTTPException(
-        status_code=404,
-        detail=f"Erro: Preferência {nome_preferencia} não existe."
-    )
+    #Atualiza os dados
+    bancoAtlax.reference("/Preferencias").update(dados_atualizados)
+    return JSONResponse(status_code=200, content="Preferencias atualizadas.")
