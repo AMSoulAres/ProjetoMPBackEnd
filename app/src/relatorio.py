5c0085f96acdf08c322012f98efaed1b212a93f0
Gabriel Amorim Soares e Silva <gabrielgabel03@gmail.com>
2023-07-06 17:26:34 -0300

    adicionando geração de relatório

--- app/src/relatorio.py
+++ app/src/relatorio.py
@@ -1,7 +1,8 @@
 """Importando módulos básicos para conexão com DBcd"""
 from typing import Optional
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from fastapi.responses import JSONResponse
+from fastapi.responses import FileResponse
 from app.src.utils.busca_usuario import busca_usuario_id
 from app.src.models.preferenciasgeral_model import PreferenciasGeralModel
 from app.src.config_db import bancoAtlax
@@ -13,19 +14,41 @@ router = APIRouter(
 
 @router.get("/{id_usuario}")
 async def relatorio(id_usuario: int):
+    """
+    Gera relatório
 
+    Asseriva de entrada: recebe o id do usuário para confirmar
+    que é um administrador
+
+    Assertiva de saída: Gera um relatório e envia um .txt baixável
+    """
     usuarios = bancoAtlax.reference("/Usuarios").get()
     usuario_base = busca_usuario_id(id_usuario, usuarios)
-
     if usuario_base["admin"] == 1:
         total_usuarios = 0
         total_preferencias = 0
         total_grupos = 0
         total_chat_privado = 0
-        total_chat_geral = 0
         for usuario in usuarios.values():
             total_usuarios += 1
 
-        preferencias = bancoAtlax.reference("/Preferencias").get()
+        total_preferencias = len(bancoAtlax.reference("/Preferencias").get()["preferencias"])
+        grupos = bancoAtlax.reference("/Grupos").get()
+
+        for key, grupo in grupos.items():
+            total_grupos += 1
         
-    return preferencias["preferencias"]
+        todasMensagens = bancoAtlax.reference('/ChatPrivado').get()       
+        for key, mensagem in todasMensagens.items():
+            total_chat_privado += 1
+        
+        with open("relatorio.txt", "a") as arquivo:
+            lines = f"Total de usuários cadastrados: {total_usuarios}\n"    
+            lines += f"Total de preferencias cadastradas: {total_preferencias}\n"
+            lines += f"Total de Grupos cadastrados: {total_grupos}\n"
+            lines += f"Total de de mensagens privadas enviadas: {total_chat_privado}"
+
+            arquivo.write(lines)
+
+        return FileResponse(path="relatorio.txt", media_type="text/plain", filename="relatorio")
+    raise HTTPException(status_code=403, detail="Usuário não é administrador")
