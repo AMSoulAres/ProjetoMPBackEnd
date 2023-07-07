e98a7443e9b039974f0bb045f1b07600b8a119b6
AdriEve <108773502+AdriEve@users.noreply.github.com>
2023-07-06 21:36:32 -0300

    Adicionando teste ao ChatPrivado - criacao dos chats

--- app/tests/test_crud_chatPrivado.py
+++ app/tests/test_crud_chatPrivado.py
@@ -7,21 +7,29 @@ client = TestClient(app)
 
 """ ------------------------- TESTE GET -------------------------  """
 
+
 def test_busca_mensagem_por_id_sucesso():
     """Teste"""
     response = client.get("/ChatPrivado/buscar-mensagens/5/3")
     assert response.status_code == 200
     assert response.json()
 
-def test_busca_mensagem_por_idR_erro():
+
+def test_busca_mensagem_idR_erro():
     """Teste"""
     response = client.get("/ChatPrivado/buscar-mensagens/300/1")
     assert response.status_code == 404
     assert response.json()
 
-def test_busca_mensagem_por_idR_erro():
+def test_busca_mensagem_idD_erro():
     """Teste"""
     response = client.get("/ChatPrivado/buscar-mensagens/1/300")
+    assert response.status_code == 404
+    assert response.json()
+ 
+def test_busca_mensagem_ids_erro():
+    """Teste"""
+    response = client.get("/ChatPrivado/buscar-mensagens/200/300")
     assert response.status_code == 404
     assert response.json()
 
@@ -29,7 +37,7 @@ def test_busca_mensagem_por_idR_erro():
 
 def test_cria_chat_sucesso():
     """Teste"""
-    response = client.post("/ChatPrivado/enviar-mensagem/0/2",
+    response = client.post("/ChatPrivado/enviar-mensagem/5/3",
                            json={
                                     "mensagem": "testando"
                            })
@@ -37,4 +45,36 @@ def test_cria_chat_sucesso():
     assert response.json() == {
         "message": "Mensagem enviada com sucesso!"
     }
+    
+def test_cria_chat_idR_erro():
+    """Teste"""
+    response = client.post("/ChatPrivado/enviar-mensagem/300/1",
+                           json={
+                                    "mensagem": "test1"
+                           })
+    assert response.status_code == 404
+    assert response.json() == {
+        "detail": "Erro: Usuário de id 300 não encontrado."
+    }
 
+def test_cria_chat_idD_erro():
+    """Teste"""
+    response = client.post("/ChatPrivado/enviar-mensagem/1/300",
+                           json={
+                                    "mensagem": "test2"
+                           })
+    assert response.status_code == 404
+    assert response.json() == {
+        "detail": "Erro: Usuário de id 300 não encontrado."
+    }
+
+def test_cria_chat_ids_erro():
+    """Teste"""
+    response = client.post("/ChatPrivado/enviar-mensagem/500/302",
+                           json={
+                                    "mensagem": "test3"
+                           })
+    assert response.status_code == 404
+    assert response.json() == {
+        "detail": "Erro: Usuário de id 500 não encontrado."
+    }
