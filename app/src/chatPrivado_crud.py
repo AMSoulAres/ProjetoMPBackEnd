64f56b6e6b8056f36361d4d41616b9d0139d5322
AdriEve <108773502+AdriEve@users.noreply.github.com>
2023-07-06 21:45:49 -0300

    alterando codigo ChatPrivado - analisando ids nao existentes

--- app/src/chatPrivado_crud.py
+++ app/src/chatPrivado_crud.py
@@ -15,9 +15,12 @@ router = APIRouter(
     responses={404: {"description": "Not Found"}}
 )
 
+""" ------------------------- READ -------------------------"""
+
+
 @router.get("/buscar-mensagens/{idRUsuario}/{idDUsuario}")
 async def buscar_mensagens(idRUsuario: int, idDUsuario: int):
-    
+        
     usuarios = bancoAtlax.reference("/Usuarios").get()
     try:
         busca_usuario_id(idRUsuario, usuarios)
@@ -38,19 +41,28 @@ async def buscar_mensagens(idRUsuario: int, idDUsuario: int):
         raise exception
     
 
+""" ------------------------- CREATE -------------------------"""
+
 
 @router.post("/enviar-mensagem/{idRUsuario}/{idDUsuario}")
 async def enviar_mensagem(dados: ChatPrivadoModel, idRUsuario: int, idDUsuario: int):
-    total_id = bancoAtlax.reference("/ChatPrivado/Total").child("num").get()
-    body = json.loads(dados.json())
-    body["idRUsuario"] = idRUsuario
-    body["idDUsuario"] = idDUsuario
-    body["timestamp"] = str(datetime.now()) if body["timestamp"] == None else body["timestamp"]
-    
-    bancoAtlax.reference("/ChatPrivado").push(body)
-    bancoAtlax.reference("/ChatPrivado").child("Total").update({"num": total_id + 1})
-    
-    return JSONResponse(
-        status_code =201,
-        content= {"message": "Mensagem enviada com sucesso!"}
-    )
+
+    usuarios = bancoAtlax.reference("/Usuarios").get()
+    try:
+        busca_usuario_id(idRUsuario, usuarios)
+        busca_usuario_id(idDUsuario, usuarios)
+        total_id = bancoAtlax.reference("/ChatPrivado/Total").child("num").get()
+        body = json.loads(dados.json())
+        body["idRUsuario"] = idRUsuario
+        body["idDUsuario"] = idDUsuario
+        body["timestamp"] = str(datetime.now()) if body["timestamp"] == None else body["timestamp"]
+        
+        bancoAtlax.reference("/ChatPrivado").push(body)
+        bancoAtlax.reference("/ChatPrivado").child("Total").update({"num": total_id + 1})
+        
+        return JSONResponse(
+            status_code =201,
+            content= {"message": "Mensagem enviada com sucesso!"}
+        )
+    except HTTPException as exception:
+        raise exception
