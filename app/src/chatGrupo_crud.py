2c7149e08aec40769d20f8f9dd4eb9dc4b0e0eff
AnalyceRay <101071397+AnalyceRay@users.noreply.github.com>
2023-07-09 07:47:28 -0300

    Criando a função de busca de mensagens

---
+++ app/src/chatGrupo_crud.py
@@ -0,0 +1,24 @@
+"""Importando módulos básicos para conexão com DBcd"""
+import json
+from fastapi import APIRouter
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import HTTPException
+from app.src.models.chatgrupo_model import GrupoMensagemModel, MensagemModel
+from app.src.config_db import bancoAtlax
+from app.src.utils.busca_grupo import busca_grupo_id
+from app.src.utils.busca_usuario import busca_usuario_id
+from datetime import datetime
+
+router = APIRouter(
+    prefix="/ChatGrupo",
+    tags=["Chat Grupo"],
+    responses={404: {"description": "Not Found"}}
+)
+
+grupos_mensagens = {}
+
+@router.get("/grupos_mensagens/{idGrupo}/mensagens/")
+async def buscar_grupo_message(idGrupo: int):
+  if idGrupo not in grupos_mensagens:
+    grupos_mensagens[idGrupo] = []
+  return grupos_mensagens[idGrupo] 
