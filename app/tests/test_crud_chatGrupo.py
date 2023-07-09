2c08f9b09622e943f895ee26f7cb55bcec2971c4
AnalyceRay <101071397+AnalyceRay@users.noreply.github.com>
2023-07-09 08:46:03 -0300

    Adicionando teste de verificação de grupos

--- app/tests/test_crud_chatGrupo.py
+++ app/tests/test_crud_chatGrupo.py
@@ -30,3 +30,14 @@ def test_enviar_grupo_menssage():
     response = client.post("/grupos_mensagens/254/5/", {"id": 5, "timestamp":"string", "mensagem": "Olá!"})
     assert response.status_code == 200
     assert response.json() == {'mensagem enviada com sucesso!'}
+
+def test_atualizar_existencia_grupo():
+    # Teste para verificar se o grupo é atualizado com sucesso.
+    response = client.put("/grupos/1/")
+    assert response.status_code == 200
+    assert response.json() == grupos_mensagens[1]
+    
+    # Teste para verificar se o grupo é excluído com sucesso.
+    response = client.put("/grupos/2/")
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'Erro: Grupo não existe mais.'}
